1b83c656ff602e8b5a76afa73f091ad0481d1997
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-09 16:59:27 +0300

    Linked project folders to index.js for improved code organization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Nav from "./components/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./components/forms/Register";
+import Login from "./components/forms/Login";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Nav />
+
+      <Routes>
+        <Route path="/" exact="true" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
