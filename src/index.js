d98e6d9ad9850c8554dd30a2750ab1f23a90e6df
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-11 11:42:06 +0300

    Added ChakraProvider, React Router, and theme toggle functionality

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Nav from "./components/Nav";
+import ReactDOM from "react-dom";
+import { ChakraProvider, CSSReset, IconButton, useColorMode } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import { FaSun, FaMoon } from "react-icons/fa";
 import Register from "./components/forms/Register";
 import Login from "./components/forms/Login";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+function ThemeToggle() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <IconButton
+      icon={colorMode === "dark" ? <FaSun /> : <FaMoon />}
+      onClick={toggleColorMode}
+      size="md"
+      variant="ghost"
+      aria-label="Toggle Theme"
+    />
+  );
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Nav />
-
-      <Routes>
-        <Route path="/" exact="true" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <ChakraProvider>
+      <CSSReset />
+      <BrowserRouter>
+        <Nav />
+        <ThemeToggle /> {/* Add the ThemeToggle component */}
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
