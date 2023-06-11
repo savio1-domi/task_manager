8ee110449219bb65370ddce6e3ba5b347b85037f
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-11 12:09:08 +0300

    Commented out NavLink components in Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+// Import other necessary components or modules
+
+function Nav() {
+  return (
+    <Box as="nav" className="Navbar" /* Add additional Chakra UI props here */>
+      {/* Add Chakra UI components and styles as needed */}
+      {/* <NavLink to="/register">Register</NavLink>
+      <NavLink to="/login">Login</NavLink> */}
+    </Box>
+  );
+}
+
+export default Nav;
