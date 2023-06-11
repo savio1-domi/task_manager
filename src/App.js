8e73ebd1dcb8da5f5f1e373e992ac2d483038dcf
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-11 11:45:16 +0300

    Updated App component with ChakraProvider and ColorModeScript

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import './App.css';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <ColorModeScript />
+      <Home />
+    </ChakraProvider>
   );
 }
 
